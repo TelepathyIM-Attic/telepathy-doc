cd0186c9982501053319a1d58dd84c532b72718e
Danielle Madeley <danielle.madeley@collabora.co.uk>
2009-11-09 14:22:32 +1100

    Implementing telepathy clients in Python

--- docs/examples/python_mc5_clients/observer.py
+++ docs/examples/python_mc5_clients/observer.py
@@ -1,61 +1,49 @@
 import dbus.glib
 import gobject
-import telepathy
 
-from telepathy._generated.Client_Observer import ClientObserver
-from telepathy.server.properties import DBusProperties
+import telepathy
 from telepathy.interfaces import CLIENT, \
                                  CLIENT_OBSERVER, \
                                  CHANNEL
 
-class ExampleObserver(ClientObserver, DBusProperties):
-    properties = {
-        CLIENT: {
-            'Interfaces': [ CLIENT_OBSERVER ],
-        },
-        CLIENT_OBSERVER: {
-            'ObserverChannelFilter': dbus.Array([
+# begin ex.services.python.object
+class ExampleObserver(telepathy.server.ClientObserver,
+                      telepathy.server.DBusProperties):
+
+    def __init__(self, *args):
+        telepathy.server.ClientObserver.__init__(self, *args)
+        telepathy.server.DBusProperties.__init__(self)
+
+        self._implement_property_get(CLIENT, {
+            'Interfaces': lambda: [ CLIENT_OBSERVER ],
+          })
+        self._implement_property_get(CLIENT_OBSERVER, {
+            'ObserverChannelFilter': lambda: dbus.Array([
                     dbus.Dictionary({
                     }, signature='sv')
                 ], signature='a{sv}')
-        },
-    }
-
-    def __init__(self, client_name):
-        bus_name = '.'.join ([CLIENT, client_name])
-        object_path = '/' + bus_name.replace('.', '/')
-
-        bus_name = dbus.service.BusName(bus_name, bus=dbus.SessionBus())
-        ClientObserver.__init__(self, bus_name, object_path)
-
-    def GetAll(self, interface):
-        print "GetAll", interface
-        if interface in self.properties:
-            return self.properties[interface]
-        else:
-            return {}
-
-    def Get(self, interface, property):
-        print "Get", interface, property
-        if interface in self.properties and \
-           property in self.properties[interface]:
-            return self.properties[interface][property]
-        else:
-            return 0
+          })
 
     def ObserveChannels(self, account, connection, channels, dispatch_operation,
                         requests_satisfied, observer_info):
-
         print "Incoming channels on %s:" % (connection)
         for object, props in channels:
             print " - %s :: %s" % (props[CHANNEL + '.ChannelType'],
                                    props[CHANNEL + '.TargetID'])
+# end ex.services.python.object
 
-def start():
-    ExampleObserver("ExampleObserver")
+# begin ex.services.python.publishing
+def publish(client_name):
+    bus_name = '.'.join ([CLIENT, client_name])
+    object_path = '/' + bus_name.replace('.', '/')
+
+    bus_name = dbus.service.BusName(bus_name, bus=dbus.SessionBus())
+
+    ExampleObserver(bus_name, object_path)
+# end ex.services.python.publishing
     return False
 
 if __name__ == '__main__':
-    gobject.timeout_add(0, start)
+    gobject.timeout_add(0, publish, "ExampleObserver")
     loop = gobject.MainLoop()
     loop.run()
